6317929a926ae801ffa41e0df7a6820f8c659b2e
sorrowise <576422965@qq.com>
2016-10-20 20:02:22 +0800

    sync

--- run.bat
+++
@@ -1,6 +0,0 @@
-python greenhat.py 5 2016-10-01
-
-"echo '" + curdate + str(randint(0, 1000000)) +"' > realwork.txt;
-git add realwork.txt;
-GIT_AUTHOR_DATE='" + curdate + "' GIT_COMMITTER_DATE='" + curdate + "' git commit -m 'update';
-git push;"
