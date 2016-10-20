aa7e2dff2a1101b3947c904a181d195bcc220d6a
sorrowise <576422965@qq.com>
2016-10-20 19:56:37 +0800

    'sync'

--- run.bat
+++ run.bat
@@ -1,1 +1,6 @@
 python greenhat.py 5 2016-10-01
+
+"echo '" + curdate + str(randint(0, 1000000)) +"' > realwork.txt;
+git add realwork.txt;
+GIT_AUTHOR_DATE='" + curdate + "' GIT_COMMITTER_DATE='" + curdate + "' git commit -m 'update';
+git push;"
--- test.py
+++ test.py
@@ -1,1 +1,19 @@
-print "echo '"+"' > realwork.txt\n"
+from datetime import date, timedelta
+from random import randint
+from time import sleep
+import sys
+import subprocess
+import os
+
+
+def get_date_string(n, startdate):
+	d = startdate - timedelta(days=n)
+	rtn = d.strftime("%a %b %d %X %Y %z -0400")
+	return rtn
+argv = ['5','2010-10-01']
+startdate = date(int(argv[1][0:4]), int(argv[1][5:7]), int(argv[1][8:10]))
+curdate = get_date_string(5,startdate)
+
+#print "echo '" + curdate + str(randint(0, 1000000)) +"' > realwork.txt"
+#print "GIT_AUTHOR_DATE='" + curdate + "' GIT_COMMITTER_DATE='" + curdate + "' git commit -m 'update'"
+print "echo '" + curdate + str(randint(0, 1000000)) +"' > realwork.txt; git add realwork.txt; GIT_AUTHOR_DATE='" + curdate + "' GIT_COMMITTER_DATE='" + curdate + "' git commit -m 'update'; git push;"
